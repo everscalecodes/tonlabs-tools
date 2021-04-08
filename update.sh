94aa10ffa6ddf5eda94529762f087cb5408eabe5
Kok Kek <kokkekpek@gmail.com>
2021-04-08 23:58:34 +0300

    Replace "-j10" to ""-j$(nproc)""

--- update.sh
+++ update.sh
@@ -3,14 +3,14 @@
 cd ~/Projects/tonlabs/tonos-cli
 git pull
 cargo update
-cargo build --release -j10
+cargo build --release -j$(nproc)
 
 # TVM-linker
 cd ~/Projects/tonlabs/TVM-linker
 git pull
 cd tvm_linker
 cargo update
-cargo build --release -j10
+cargo build --release -j$(nproc)
 
 # TON-Solidity-Compiler
 cd ~/Projects/tonlabs/TON-Solidity-Compiler
@@ -19,4 +19,4 @@ rm -rf build
 mkdir build
 cd build
 cmake ../compiler/ -DCMAKE_BUILD_TYPE=Release
-cmake --build . -- -j10
+cmake --build . -- -j$(nproc)
