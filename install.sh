f6a9345ac6dd72dea0cebdebd81871921d94d108
Nikita <66723320+kokkekpek@users.noreply.github.com>
2021-09-12 01:13:54 +0300

    Update install.sh
    Replace `tonos-cli` alias to `tonos-cli-sa`

--- install.sh
+++ install.sh
@@ -18,6 +18,6 @@ cd ~/Projects/tonlabs/TON-Solidity-Compiler
 sh ./compiler/scripts/install_deps.sh
 
 # Aliases
-echo "alias tonos-cli='~/Projects/tonlabs/tonos-cli/target/release/tonos-cli'" >> ~/.bash_aliases
+echo "alias tonos-cli-sa='~/Projects/tonlabs/tonos-cli/target/release/tonos-cli'" >> ~/.bash_aliases
 echo "alias tvm_linker='~/Projects/tonlabs/TVM-linker/tvm_linker/target/release/tvm_linker'" >> ~/.bash_aliases
 echo "alias solc='~/Projects/tonlabs/TON-Solidity-Compiler/build/solc/solc'" >> ~/.bash_aliases
